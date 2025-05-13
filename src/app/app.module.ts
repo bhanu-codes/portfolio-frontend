8ab0079738984df6eb924828659b9bb0146c4f14
bhanu-codes <bhanuneeli2930@gmail.com>
2025-05-13 22:45:12 +0530

    feat: Added required components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
 import {NavbarComponent} from './navbar/navbar.component';
 import {HomeComponent} from './home/home.component';
 import {AboutComponent} from './about/about.component';
@@ -9,21 +11,14 @@ import {ExperienceComponent} from './experience/experience.component';
 import {ContactComponent} from './contact/contact.component';
 import {FooterComponent} from './footer/footer.component';
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet,
-    NavbarComponent,
-    HomeComponent,
-    AboutComponent,
-    SkillsComponent,
-    ProjectsComponent,
-    ExperienceComponent,
-    ContactComponent,
-    FooterComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppComponent {
-  title = 'portfolio-frontend';
-}
+export class AppModule { }
