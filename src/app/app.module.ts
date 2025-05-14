0f8ce3c7dd74a2cf60180f3584910acc1e558934
bhanu-codes <bhanuneeli2930@gmail.com>
2025-05-15 01:09:56 +0530

    feat: gh pages added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {NavbarComponent} from './navbar/navbar.component';
-import {HomeComponent} from './home/home.component';
-import {AboutComponent} from './about/about.component';
-import {SkillsComponent} from './skills/skills.component';
-import {ProjectsComponent} from './projects/projects.component';
-import {ExperienceComponent} from './experience/experience.component';
-import {ContactComponent} from './contact/contact.component';
-import {FooterComponent} from './footer/footer.component';
 
 @NgModule({
   declarations: [
